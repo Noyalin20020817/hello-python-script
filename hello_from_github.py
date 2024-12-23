b361ff76963d9050486d4e6176d3928da090aa1d
Noyalin20020817 <noyalinromy@gmail.com>
2024-12-23 13:47:11 +0530

    Update hello_from_github.py

--- hello_from_github.py
+++ hello_from_github.py
@@ -1,16 +1,2 @@
 # hello-python-scr# hello.py
 print("Hello, World!")
-import requests
-
-# Replace with your GitHub raw file URL
-url = "https://raw.githubusercontent.com/yourusername/my-python-scripts/main/hello.py"
-
-response = requests.get(url)
-
-if response.status_code == 200:
-    with open('hello_from_github.py', 'w') as f:
-        f.write(response.text)
-    print("File downloaded successfully!") 
-else:
-    print("Failed to fetch the file.")
-  
