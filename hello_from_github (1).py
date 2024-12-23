594a05cf27e2ac21745e22298ce071c05182dcc1
Noyalin20020817 <noyalinromy@gmail.com>
2024-12-23 14:42:13 +0530

    hello.py

--- hello_from_github (1).py
+++ hello_from_github (1).py
@@ -1,2 +1,11 @@
 # hello-py
-print("Hello, World!")
+print("Hello, World!")import requests
+
+url = "https://raw.githubusercontent.com/Noyalin20020817/hello-python-script/main/hello.py"
+response = requests.get(url)
+
+if response.status_code == 200:
+    script_content = response.text
+    print("Fetched script content:\n", script_content)
+else:
+    print("Failed to fetch the script. Status code:", response.status_code)
