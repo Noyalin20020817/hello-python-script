6e51cfa25a463113bc446b5b1522e791ac68096f
Noyalin20020817 <noyalinromy@gmail.com>
2024-12-23 20:07:44 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,6 +2,21 @@
 print("Hello, World!")
 import requests
 
+repository_link = "https://github.com/Noyalin20020817/hello-python-script"
+python_code = '''print("Hello, World!")
+import requests
+
+repository_link = "https://github.com/Noyalin20020817/hello-python-script"
+python_code = "Your code here"
+
+with open("output.txt", "w") as file:
+    file.write(f"GitHub Repository Link: {repository_link}\\n\\n")
+    file.write("Python Code:\\n")
+    file.write(python_code)
+
+print("output.txt file has been created.")
+'''
+
 url = "https://raw.githubusercontent.com/Noyalin20020817/hello-python-script/main/hello.py"
 response = requests.get(url)
 
